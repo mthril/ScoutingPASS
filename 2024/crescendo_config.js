6ae56381d305438cf5670a635b858e43c255ac9c
mthril <slee87944@gmail.com>
2024-09-10 19:42:38 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -2,7 +2,7 @@ var config_data = `
 {
   "dataFormat": "tsv",
   "title": "Scouting PASS 9999",
-  "page_title": "Crescendo",
+  "page_title": "TestTest",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
